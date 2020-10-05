f802ae37cebf110835a6f69702baef30fef31d5d
collerek <collerek@gmail.com>
2020-10-05 10:08:51 +0200

    update readme, bump version

--- tests/test_foreign_keys.py
+++ tests/test_foreign_keys.py
@@ -233,8 +233,8 @@ async def test_fk_filter():
 
             tracks = (
                 await Track.objects.select_related("album")
-                    .filter(album__name="Fantasies")
-                    .all()
+                .filter(album__name="Fantasies")
+                .all()
             )
             assert len(tracks) == 3
             for track in tracks:
@@ -242,8 +242,8 @@ async def test_fk_filter():
 
             tracks = (
                 await Track.objects.select_related("album")
-                    .filter(album__name__icontains="fan")
-                    .all()
+                .filter(album__name__icontains="fan")
+                .all()
             )
             assert len(tracks) == 3
             for track in tracks:
@@ -288,8 +288,8 @@ async def test_multiple_fk():
 
             members = (
                 await Member.objects.select_related("team__org")
-                    .filter(team__org__ident="ACME Ltd")
-                    .all()
+                .filter(team__org__ident="ACME Ltd")
+                .all()
             )
             assert len(members) == 4
             for member in members:
@@ -321,8 +321,8 @@ async def test_pk_filter():
 
             tracks = (
                 await Track.objects.select_related("album")
-                    .filter(position=2, album__name="Test")
-                    .all()
+                .filter(position=2, album__name="Test")
+                .all()
             )
             assert len(tracks) == 1
 
--- tests/test_selecting_subset_of_columns.py
+++ tests/test_selecting_subset_of_columns.py
@@ -50,33 +50,69 @@ async def test_selecting_subset():
     async with database:
         async with database.transaction(force_rollback=True):
             toyota = await Company.objects.create(name="Toyota", founded=1937)
-            await Car.objects.create(manufacturer=toyota, name="Corolla", year=2020, gearbox_type='Manual', gears=5,
-                                     aircon_type='Manual')
-            await Car.objects.create(manufacturer=toyota, name="Yaris", year=2019, gearbox_type='Manual', gears=5,
-                                     aircon_type='Manual')
-            await Car.objects.create(manufacturer=toyota, name="Supreme", year=2020, gearbox_type='Auto', gears=6,
-                                     aircon_type='Auto')
+            await Car.objects.create(
+                manufacturer=toyota,
+                name="Corolla",
+                year=2020,
+                gearbox_type="Manual",
+                gears=5,
+                aircon_type="Manual",
+            )
+            await Car.objects.create(
+                manufacturer=toyota,
+                name="Yaris",
+                year=2019,
+                gearbox_type="Manual",
+                gears=5,
+                aircon_type="Manual",
+            )
+            await Car.objects.create(
+                manufacturer=toyota,
+                name="Supreme",
+                year=2020,
+                gearbox_type="Auto",
+                gears=6,
+                aircon_type="Auto",
+            )
 
-            all_cars = await Car.objects.select_related('manufacturer').fields(['id', 'name', 'company__name']).all()
+            all_cars = (
+                await Car.objects.select_related("manufacturer")
+                .fields(["id", "name", "company__name"])
+                .all()
+            )
             for car in all_cars:
-                assert all(getattr(car, x) is None for x in ['year', 'gearbox_type', 'gears', 'aircon_type'])
-                assert car.manufacturer.name == 'Toyota'
+                assert all(
+                    getattr(car, x) is None
+                    for x in ["year", "gearbox_type", "gears", "aircon_type"]
+                )
+                assert car.manufacturer.name == "Toyota"
                 assert car.manufacturer.founded is None
 
-            all_cars = await Car.objects.select_related('manufacturer').fields('id').fields(
-                ['name', 'manufacturer']).all()
+            all_cars = (
+                await Car.objects.select_related("manufacturer")
+                .fields("id")
+                .fields(["name"])
+                .all()
+            )
             for car in all_cars:
-                assert all(getattr(car, x) is None for x in ['year', 'gearbox_type', 'gears', 'aircon_type'])
-                assert car.manufacturer.name == 'Toyota'
+                assert all(
+                    getattr(car, x) is None
+                    for x in ["year", "gearbox_type", "gears", "aircon_type"]
+                )
+                assert car.manufacturer.name == "Toyota"
                 assert car.manufacturer.founded == 1937
 
-            all_cars_check = await Car.objects.select_related('manufacturer').all()
+            all_cars_check = await Car.objects.select_related("manufacturer").all()
             for car in all_cars_check:
-                assert all(getattr(car, x) is not None for x in ['year', 'gearbox_type', 'gears', 'aircon_type'])
-                assert car.manufacturer.name == 'Toyota'
+                assert all(
+                    getattr(car, x) is not None
+                    for x in ["year", "gearbox_type", "gears", "aircon_type"]
+                )
+                assert car.manufacturer.name == "Toyota"
                 assert car.manufacturer.founded == 1937
 
             with pytest.raises(pydantic.error_wrappers.ValidationError):
                 # cannot exclude mandatory model columns - company__name in this example
-                await Car.objects.select_related('manufacturer').fields(
-                    ['id', 'name', 'company__founded']).all()
+                await Car.objects.select_related("manufacturer").fields(
+                    ["id", "name", "company__founded"]
+                ).all()
