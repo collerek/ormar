551aabf0a8ff61a2c28c889719c8e92c9c2c6276
collerek <collerek@gmail.com>
2020-11-01 18:03:18 +0700

    Update relation_protocol.py

--- ormar/protocols/relation_protocol.py
+++ ormar/protocols/relation_protocol.py
@@ -1,4 +1,9 @@
-from typing import Protocol, TYPE_CHECKING, Type, Union
+from typing import TYPE_CHECKING, Type, Union
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extentions import Protocol
 
 if TYPE_CHECKING:  # pragma: nocover
     from ormar import Model
