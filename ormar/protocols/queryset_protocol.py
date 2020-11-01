9bcf6a346adf55be4c7c139c38ed1ca0038c1015
collerek <collerek@gmail.com>
2020-11-01 18:02:25 +0700

    Update queryset_protocol.py

--- ormar/protocols/queryset_protocol.py
+++ ormar/protocols/queryset_protocol.py
@@ -1,4 +1,9 @@
-from typing import Any, List, Optional, Protocol, Sequence, TYPE_CHECKING, Union
+from typing import Any, List, Optional, Sequence, TYPE_CHECKING, Union
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extentions import Protocol
 
 if TYPE_CHECKING:  # noqa: C901; #pragma nocover
     from ormar import QuerySet, Model
