e56b0b5c495971f9960c1bae0485585a9dac0b01
collerek <collerek@gmail.com>
2021-02-10 17:55:35 +0100

    remove unused tests for now

--- tests/test_m2m_through_fields.py
+++ tests/test_m2m_through_fields.py
@@ -38,28 +38,28 @@ class Post(ormar.Model):
     title: str = ormar.String(max_length=200)
     categories = ormar.ManyToMany(Category, through=PostCategory)
 
-
-@pytest.fixture(autouse=True, scope="module")
-async def create_test_database():
-    engine = sqlalchemy.create_engine(DATABASE_URL)
-    metadata.create_all(engine)
-    yield
-    metadata.drop_all(engine)
-
-
-@pytest.mark.asyncio
-async def test_setting_fields_on_through_model():
-    async with database:
-        # TODO: check/ modify following
-        # loading the data into model instance of though model?
-        # <- attach to other side? both sides? access by through, or add to fields?
-        # creating while adding to relation (kwargs in add?)
-        # creating in query (dividing kwargs between final and through)
-        # updating in query
-        # sorting in filter (special __through__<field_name> notation?)
-        # ordering by in order_by
-        # accessing from instance (both sides?)
-        # modifying from instance (both sides?)
-        # including/excluding in fields?
-        # allowing to change fk fields names in through model?
-        pass
+#
+# @pytest.fixture(autouse=True, scope="module")
+# async def create_test_database():
+#     engine = sqlalchemy.create_engine(DATABASE_URL)
+#     metadata.create_all(engine)
+#     yield
+#     metadata.drop_all(engine)
+#
+#
+# @pytest.mark.asyncio
+# async def test_setting_fields_on_through_model():
+#     async with database:
+#         # TODO: check/ modify following
+#         # loading the data into model instance of though model?
+#         # <- attach to other side? both sides? access by through, or add to fields?
+#         # creating while adding to relation (kwargs in add?)
+#         # creating in query (dividing kwargs between final and through)
+#         # updating in query
+#         # sorting in filter (special __through__<field_name> notation?)
+#         # ordering by in order_by
+#         # accessing from instance (both sides?)
+#         # modifying from instance (both sides?)
+#         # including/excluding in fields?
+#         # allowing to change fk fields names in through model?
+#         pass
