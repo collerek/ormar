869f4d9d9755b0f7aae229140dcdcb9ff418defc
collerek <collerek@gmail.com>
2021-03-09 20:49:20 +0100

    restore uuid check for fk

--- ormar/fields/foreign_key.py
+++ ormar/fields/foreign_key.py
@@ -399,6 +399,8 @@ class ForeignKeyField(BaseField):
         :return: (if needed) registered Model
         :rtype: Model
         """
+        if cls.to.pk_type() == uuid.UUID and isinstance(value, str):
+            value = uuid.UUID(value)
         if not isinstance(value, cls.to.pk_type()):
             raise RelationshipInstanceError(
                 f"Relationship error - ForeignKey {cls.to.__name__} "
