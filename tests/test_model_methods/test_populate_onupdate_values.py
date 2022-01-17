083780b622a2c601a2f8b4a38ea8b396fb0d6b98
huangsong <huangsong@leyantech.com>
2022-01-17 12:38:51 +0800

    with database

--- tests/test_model_methods/test_populate_onupdate_values.py
+++ tests/test_model_methods/test_populate_onupdate_values.py
@@ -40,32 +40,34 @@ def create_test_database():
 
 @pytest.mark.asyncio
 async def test_populate_onupdate_values():
-    task = Task(name="123", age=1, points=1)
-    await task.save()
+    async with database:
+        task = Task(name="123", age=1, points=1)
+        await task.save()
 
-    assert task.year == 1
-    await task.update(age=2)
+        assert task.year == 1
+        await task.update(age=2)
 
-    t = await Task.objects.filter(age=2).first()
-    assert t.name == "hello"
-    assert t.points == 1
-    assert t.year == 2
+        t = await Task.objects.filter(age=2).first()
+        assert t.name == "hello"
+        assert t.points == 1
+        assert t.year == 2
 
 
 @pytest.mark.asyncio
 async def test_bulk_update_populate_onupdate_values():
-    task1 = await Task(name="123", age=1, points=2).save()
-    task2 = await Task(name="123", age=2, points=3).save()
-    task3 = await Task(name="123", age=3, points=4).save()
+    async with database:
+        task1 = await Task(name="123", age=1, points=2).save()
+        task2 = await Task(name="123", age=2, points=3).save()
+        task3 = await Task(name="123", age=3, points=4).save()
 
-    tasks = [task1, task2, task3]
+        tasks = [task1, task2, task3]
 
-    for task in tasks:
-        task.age += 1
+        for task in tasks:
+            task.age += 1
 
-    await Task.objects.bulk_update(tasks)
+        await Task.objects.bulk_update(tasks)
 
-    for task in await Task.objects.all():
-        assert task.points == 1
-        assert task.name == "hello"
-        assert task.year == 2
+        for task in await Task.objects.all():
+            assert task.points == 1
+            assert task.name == "hello"
+            assert task.year == 2
