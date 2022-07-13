4f59ff48c6f58b42c79b63d6b4e64c7b2a8d5426
Sepehr <bazyar.sepehr@gmail.com>
2022-07-13 09:05:11 +0430

    refactor: edit check name replace sapce underline

--- ormar/models/helpers/sqlalchemy.py
+++ ormar/models/helpers/sqlalchemy.py
@@ -318,7 +318,8 @@ def set_constraint_names(meta: "ModelMeta") -> None:
                 f'{"_".join([col for col in constraint._pending_colargs])}'
             )
         elif isinstance(constraint, sqlalchemy.CheckConstraint) and not constraint.name:
-            constraint.name = f"check_{meta.tablename}_{constraint.sqltext}"
+            sql_condition: str = constraint.sqltext.replace(" ", "_")
+            constraint.name = f"check_{meta.tablename}_{sql_condition}"
 
 
 def update_column_definition(
