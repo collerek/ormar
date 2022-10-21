0298b71e05d7c6ab40613b3294c46c7530da2533
collerek <collerek@gmail.com>
2022-10-21 15:16:24 +0200

    Fix coverage

--- tests/test_inheritance_and_pydantic_generation/test_inheritance_of_property_fields.py
+++ tests/test_inheritance_and_pydantic_generation/test_inheritance_of_property_fields.py
@@ -26,6 +26,22 @@ class Foo(BaseFoo):
         metadata = metadata
         database = database
 
+    @ormar.property_field
+    def double_prefixed_name(self) -> str:
+        return "prefix2_" + self.name
+
+    id: int = ormar.Integer(primary_key=True)
+
+
+class Bar(BaseFoo):
+    class Meta:
+        metadata = metadata
+        database = database
+
+    @ormar.property_field
+    def prefixed_name(self) -> str:
+        return "baz_" + self.name
+
     id: int = ormar.Integer(primary_key=True)
 
 
@@ -41,4 +57,13 @@ def create_test_database():
 def test_property_fields_are_inherited():
     foo = Foo(name="foo")
     assert foo.prefixed_name == "prefix_foo"
-    assert foo.json() == '{"name": "foo", "id": null, "prefixed_name": "prefix_foo"}'
+    assert foo.dict() == {
+        "name": "foo",
+        "id": None,
+        "double_prefixed_name": "prefix2_foo",
+        "prefixed_name": "prefix_foo",
+    }
+
+    bar = Bar(name="bar")
+    assert bar.prefixed_name == "baz_bar"
+    assert bar.dict() == {"name": "bar", "id": None, "prefixed_name": "baz_bar"}
