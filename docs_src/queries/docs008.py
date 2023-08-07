911d6463c499ecc8dda54b3355cac3e4ad8dad59
Edward Betts <edward@4angle.com>
2023-08-07 13:30:31 +0100

    Correct spelling mistakes

--- docs_src/queries/docs008.py
+++ docs_src/queries/docs008.py
@@ -59,7 +59,7 @@ for car in all_cars:
 # models selected in select_related but with no columns in fields list implies all fields
 all_cars = await Car.objects.select_related('manufacturer').exclude_fields('year').exclude_fields(
     ['gear', 'gearbox_type']).all()
-# all fiels from company model are selected
+# all fields from company model are selected
 assert all_cars[0].manufacturer.name == 'Toyota'
 assert all_cars[0].manufacturer.founded == 1937
 
