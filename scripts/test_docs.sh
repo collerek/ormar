81347972f76896cf4a2a2172c5392c67884722a5
collerek <collerek@gmail.com>
2024-01-26 18:55:44 +0100

    change dir in test docs

--- scripts/test_docs.sh
+++ scripts/test_docs.sh
@@ -9,4 +9,4 @@ fi
 
 set -x
 
-PYTHONPATH=. ${PREFIX}pytest --ignore venv --cov=${PACKAGE} --cov=tests --cov-report=xml --cov-fail-under=100 --cov-report=term-missing tests/ "${@}"
+PYTHONPATH=. ${PREFIX}pytest --ignore venv --cov=${PACKAGE} --cov=tests --cov-report=xml --cov-fail-under=100 --cov-report=term-missing docs_src/ "${@}"
