6f9a54b614d04ffc9031e85901fb61e5e4247024
collerek <collerek@gmail.com>
2024-12-04 23:27:20 +0900

    Fix #1399

---
+++ tests/test_model_definition/test_through_model_relation_setup_on_clone.py
@@ -0,0 +1,33 @@
+import ormar
+import pytest
+
+from tests.lifespan import init_tests
+from tests.settings import create_config
+
+base_ormar_config = create_config()
+
+
+class Author(ormar.Model):
+    ormar_config = base_ormar_config.copy(tablename="authors")
+
+    id = ormar.Integer(primary_key=True)
+    name = ormar.String(max_length=100)
+
+
+class Book(ormar.Model):
+    ormar_config = base_ormar_config.copy(tablename="books")
+
+    id = ormar.Integer(primary_key=True)
+    title = ormar.String(max_length=100)
+    author = ormar.ManyToMany(
+        Author,
+    )
+    year = ormar.Integer(nullable=True)
+
+
+create_test_database = init_tests(base_ormar_config)
+
+
+@pytest.mark.asyncio
+async def test_tables_are_created():
+    assert await Book.objects.all() == []
