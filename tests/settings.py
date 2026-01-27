f733e67933742c4bb80cde3f7da3eaba953a90b9
hk3dva <kirya.shabalin.2014@mail.ru>
2026-01-27 04:54:06 +0300

    remove dotenv

--- tests/settings.py
+++ tests/settings.py
@@ -4,14 +4,10 @@ import databases
 import ormar
 import sqlalchemy
 
-from dotenv import load_dotenv
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///test.db")
 database_url = databases.DatabaseURL(DATABASE_URL)
 if database_url.scheme == "postgresql+aiopg":  # pragma no cover
     DATABASE_URL = str(database_url.replace(driver=None))
-print("USED DB:", DATABASE_URL)
 
 
 def create_config(**args):
