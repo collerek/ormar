44cc87c807a2b74216ad2368be8927b90f82a89d
collerek <collerek@gmail.com>
2026-02-06 22:31:15 +0700

    Revert "update benchmarks restore part of the options"
    This reverts commit 2fe38dc2fdcc335a5fcf5987ba9196ee834989ec.

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -86,7 +86,7 @@ async def authors_in_db(num_models: int):
 
 
 @pytest_asyncio.fixture
-@pytest.mark.benchmark(min_rounds=1, disable_gc=True, warmup=False)
+@pytest.mark.benchmark
 async def aio_benchmark(benchmark):
     def _fixture_wrapper(func):
         def _func_wrapper(*args, **kwargs):
